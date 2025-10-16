c4c8b5054c44e71b16e8d7cf56037974b8fe12f8
Marcin Jelinski <marcin.jelinski@nordicsemi.no>
2025-10-16 09:32:23 +0200

    wip

--- subsys/esb/esb_dppi.c
+++ subsys/esb/esb_dppi.c
@@ -9,6 +9,12 @@
 #include <hal/nrf_timer.h>
 
 #include <nrfx_dppi.h>
+/* Optional GPIO pin debugging via GPIOTE/DPPI on nRF54 (H- and L-series) */
+#if defined(CONFIG_SOC_SERIES_NRF54HX) || defined(CONFIG_SOC_SERIES_NRF54LX)
+#include <nrfx_gpiote.h>
+#include <helpers/nrfx_gppi.h>
+#include <hal/nrf_gpio.h>
+#endif
 
 #include <zephyr/logging/log.h>
 
@@ -26,6 +32,140 @@ static uint8_t egu_ramp_up;
 static uint8_t radio_end_timer_start;
 
 static nrf_dppi_channel_group_t ramp_up_dppi_group;
+
+#if defined(CONFIG_SOC_SERIES_NRF54HX) || defined(CONFIG_SOC_SERIES_NRF54LX)
+#define ESB_GPIO_DEBUG_PIN NRF_GPIO_PIN_MAP(1, 10)
+#endif
+
+#if defined(CONFIG_SOC_SERIES_NRF54LX)
+static const nrfx_dppi_t esb_dppi_radio_domain = NRFX_DPPI_INSTANCE(10);
+static const nrfx_dppi_t esb_dppi_gpio_domain  = NRFX_DPPI_INSTANCE(20);
+static const nrfx_gpiote_t esb_gpiote          = NRFX_GPIOTE_INSTANCE(20);
+#elif defined(CONFIG_SOC_SERIES_NRF54HX)
+static const nrfx_dppi_t esb_dppi_radio_domain = NRFX_DPPI_INSTANCE(020);
+static const nrfx_dppi_t esb_dppi_gpio_domain  = NRFX_DPPI_INSTANCE(020);
+static const nrfx_gpiote_t esb_gpiote          = NRFX_GPIOTE_INSTANCE(0);
+#else
+#error "No DPPI domain defined"
+#endif
+
+static uint8_t esb_dbg_gppi_bridge_ready;
+static uint8_t esb_dbg_gppi_bridge_end;
+static uint8_t esb_dbg_dppi_radio_ready;
+static uint8_t esb_dbg_dppi_radio_end;
+static uint8_t esb_dbg_dppi_gpio_ready;
+static uint8_t esb_dbg_dppi_gpio_end;
+static uint8_t esb_dbg_gpiote_chan_ready_end;
+static bool esb_dbg_gpio_active;
+
+static int esb_debug_gpio_setup(void)
+{
+    nrfx_err_t err;
+
+    if (!(IS_ENABLED(CONFIG_SOC_SERIES_NRF54HX) || IS_ENABLED(CONFIG_SOC_SERIES_NRF54LX))) {
+        return 0;
+    }
+
+    if (!nrfx_gpiote_init_check(&esb_gpiote)) {
+        err = nrfx_gpiote_init(&esb_gpiote, 0);
+        if (err != NRFX_SUCCESS) {
+            LOG_ERR("GPIOTE init failed: %d", err);
+            return -ENODEV;
+        }
+    }
+
+    err = nrfx_gpiote_channel_alloc(&esb_gpiote, &esb_dbg_gpiote_chan_ready_end);
+    if (err != NRFX_SUCCESS) {
+        LOG_ERR("GPIOTE channel alloc failed: %d", err);
+        return -ENODEV;
+    }
+
+    const nrfx_gpiote_output_config_t out_cfg = NRFX_GPIOTE_DEFAULT_OUTPUT_CONFIG;
+    const nrfx_gpiote_task_config_t task_cfg = {
+        .task_ch = esb_dbg_gpiote_chan_ready_end,
+        .polarity = NRF_GPIOTE_POLARITY_TOGGLE,
+        .init_val = NRF_GPIOTE_INITIAL_VALUE_LOW,
+    };
+
+    err = nrfx_gpiote_output_configure(&esb_gpiote, ESB_GPIO_DEBUG_PIN, &out_cfg, &task_cfg);
+    if (err != NRFX_SUCCESS) {
+        LOG_ERR("GPIOTE output configure failed: %d", err);
+        return -ENODEV;
+    }
+
+#if defined(CONFIG_SOC_SERIES_NRF54LX)
+    if (nrfx_gppi_channel_alloc(&esb_dbg_gppi_bridge_ready) != NRFX_SUCCESS ||
+        nrfx_gppi_channel_alloc(&esb_dbg_gppi_bridge_end)   != NRFX_SUCCESS) {
+        LOG_ERR("GPPI bridge channel alloc failed");
+        return -ENODEV;
+    }
+#endif
+
+    err = nrfx_dppi_channel_alloc(&esb_dppi_radio_domain, &esb_dbg_dppi_radio_ready);
+    if (err != NRFX_SUCCESS) {
+        LOG_ERR("DPPI radio READY alloc failed: %d", err);
+        return -ENODEV;
+    }
+    err = nrfx_dppi_channel_alloc(&esb_dppi_radio_domain, &esb_dbg_dppi_radio_end);
+    if (err != NRFX_SUCCESS) {
+        LOG_ERR("DPPI radio END alloc failed: %d", err);
+        return -ENODEV;
+    }
+
+    err = nrfx_dppi_channel_alloc(&esb_dppi_gpio_domain, &esb_dbg_dppi_gpio_ready);
+    if (err != NRFX_SUCCESS) {
+        LOG_ERR("DPPI gpio READY alloc failed: %d", err);
+        return -ENODEV;
+    }
+    err = nrfx_dppi_channel_alloc(&esb_dppi_gpio_domain, &esb_dbg_dppi_gpio_end);
+    if (err != NRFX_SUCCESS) {
+        LOG_ERR("DPPI gpio END alloc failed: %d", err);
+        return -ENODEV;
+    }
+
+#if defined(CONFIG_SOC_SERIES_NRF54LX)
+    err = nrfx_gppi_edge_connection_setup(esb_dbg_gppi_bridge_ready,
+                                          &esb_dppi_radio_domain, esb_dbg_dppi_radio_ready,
+                                          &esb_dppi_gpio_domain,  esb_dbg_dppi_gpio_ready);
+    if (err != NRFX_SUCCESS) {
+        LOG_ERR("GPPI bridge READY setup failed: %d", err);
+        return -ENODEV;
+    }
+    err = nrfx_gppi_edge_connection_setup(esb_dbg_gppi_bridge_end,
+                                          &esb_dppi_radio_domain, esb_dbg_dppi_radio_end,
+                                          &esb_dppi_gpio_domain,  esb_dbg_dppi_gpio_end);
+    if (err != NRFX_SUCCESS) {
+        LOG_ERR("GPPI bridge END setup failed: %d", err);
+        return -ENODEV;
+    }
+#endif
+
+    nrf_radio_publish_set(NRF_RADIO, NRF_RADIO_EVENT_READY, esb_dbg_dppi_radio_ready);
+    nrf_radio_publish_set(NRF_RADIO, ESB_RADIO_EVENT_END,   esb_dbg_dppi_radio_end);
+
+    nrf_gpiote_subscribe_set(esb_gpiote.p_reg,
+                             nrfx_gpiote_set_task_address_get(&esb_gpiote, ESB_GPIO_DEBUG_PIN),
+                             esb_dbg_dppi_gpio_ready);
+    nrf_gpiote_subscribe_set(esb_gpiote.p_reg,
+                             nrfx_gpiote_clr_task_address_get(&esb_gpiote, ESB_GPIO_DEBUG_PIN),
+                             esb_dbg_dppi_gpio_end);
+
+#if defined(CONFIG_SOC_SERIES_NRF54LX)
+    nrfx_gppi_channels_enable(NRFX_BIT(esb_dbg_gppi_bridge_ready) |
+                              NRFX_BIT(esb_dbg_gppi_bridge_end));
+#endif
+    (void)nrfx_dppi_channel_enable(&esb_dppi_radio_domain, esb_dbg_dppi_radio_ready);
+    (void)nrfx_dppi_channel_enable(&esb_dppi_radio_domain, esb_dbg_dppi_radio_end);
+    (void)nrfx_dppi_channel_enable(&esb_dppi_gpio_domain,  esb_dbg_dppi_gpio_ready);
+    (void)nrfx_dppi_channel_enable(&esb_dppi_gpio_domain,  esb_dbg_dppi_gpio_end);
+
+    nrfx_gpiote_out_task_enable(&esb_gpiote, ESB_GPIO_DEBUG_PIN);
+
+    esb_dbg_gpio_active = true;
+    return 0;
+}
+
+#endif /* CONFIG_SOC_SERIES_NRF54HX || CONFIG_SOC_SERIES_NRF54LX */
 
 void esb_ppi_for_txrx_set(bool rx, bool timer_start)
 {
@@ -270,6 +410,11 @@ int esb_ppi_init(void)
 	}
 	nrf_dppi_channels_enable(ESB_DPPIC, BIT(disabled_phy_end_egu));
 
+
+#if defined(CONFIG_SOC_SERIES_NRF54HX) || defined(CONFIG_SOC_SERIES_NRF54LX)
+	esb_debug_gpio_setup();
+#endif
+
 	return 0;
 
 #if !defined(ESB_DPPI_FIXED)
@@ -291,6 +436,20 @@ void esb_ppi_disable_all(void)
 					BIT(timer_compare1_radio_txen) : 0));
 
 	nrf_dppi_channels_disable(ESB_DPPIC, channels_mask);
+
+#if defined(CONFIG_SOC_SERIES_NRF54HX) || defined(CONFIG_SOC_SERIES_NRF54LX)
+	/* Also disable debug GPIO DPPI channels if active. */
+	if (esb_dbg_gpio_active) {
+		(void)nrfx_dppi_channel_disable(&esb_dppi_radio_domain, esb_dbg_dppi_radio_ready);
+		(void)nrfx_dppi_channel_disable(&esb_dppi_radio_domain, esb_dbg_dppi_radio_end);
+		(void)nrfx_dppi_channel_disable(&esb_dppi_gpio_domain,  esb_dbg_dppi_gpio_ready);
+		(void)nrfx_dppi_channel_disable(&esb_dppi_gpio_domain,  esb_dbg_dppi_gpio_end);
+#if defined(CONFIG_SOC_SERIES_NRF54LX)
+		nrfx_gppi_channels_disable(NRFX_BIT(esb_dbg_gppi_bridge_ready) |
+					   NRFX_BIT(esb_dbg_gppi_bridge_end));
+#endif
+	}
+#endif
 }
 
 void esb_ppi_deinit(void)
